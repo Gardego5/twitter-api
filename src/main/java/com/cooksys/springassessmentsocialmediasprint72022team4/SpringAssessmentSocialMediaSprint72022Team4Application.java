3bede4b03ab4752c036d0294c626ed504abdf95c
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-01 07:58:44 -0700

    Generate Spring Boot Project with Spring Initializr

---
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/SpringAssessmentSocialMediaSprint72022Team4Application.java
@@ -0,0 +1,13 @@
+package com.cooksys.springassessmentsocialmediasprint72022team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssessmentSocialMediaSprint72022Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssessmentSocialMediaSprint72022Team4Application.class, args);
+	}
+
+}
