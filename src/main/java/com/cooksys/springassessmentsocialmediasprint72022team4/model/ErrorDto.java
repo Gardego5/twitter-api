1292166ad8d92c82038f2a17ac0ffaf79f8ca2ee
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-01 12:11:32 -0700

    Closes #18 ErrorDto

---
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/model/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.cooksys.springassessmentsocialmediasprint72022team4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ErrorDto {
+    
+    private String message;
+}
