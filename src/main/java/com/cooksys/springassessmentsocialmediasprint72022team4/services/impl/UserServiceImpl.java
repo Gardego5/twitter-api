add69731a40d87992c8c96ae26fcaef8251f327c
Jesus Milan <milan.jesusdev@gmail.com>
2022-08-01 15:55:26 -0400

    added service implementations

---
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cooksys.springassessmentsocialmediasprint72022team4.services.impl;
+
+import com.cooksys.springassessmentsocialmediasprint72022team4.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+}
