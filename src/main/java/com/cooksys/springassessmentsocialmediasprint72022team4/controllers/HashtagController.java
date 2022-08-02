b286f7bfde09a623a31fb04f922e243cb00bef1b
Will Marttala <wmarttala123@gmail.com>
2022-08-02 12:52:50 -0500

    Update HashtagController.java

--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/controllers/HashtagController.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/controllers/HashtagController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hashtags")
+@RequestMapping("/tags")
 public class HashtagController {
 }
