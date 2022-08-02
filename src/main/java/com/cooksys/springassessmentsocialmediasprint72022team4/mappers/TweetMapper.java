a3a74fc011a403a6f0aa01056d7a0f488ebd21df
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-02 13:43:25 -0700

    Reimplement Mapper Simplification from Will

--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/mappers/TweetMapper.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/mappers/TweetMapper.java
@@ -1,31 +1,19 @@
 package com.cooksys.springassessmentsocialmediasprint72022team4.mappers;
 
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
 import com.cooksys.springassessmentsocialmediasprint72022team4.entities.Tweet;
 import com.cooksys.springassessmentsocialmediasprint72022team4.model.TweetRequestDto;
 import com.cooksys.springassessmentsocialmediasprint72022team4.model.TweetResponseDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.List;
-
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {TimeMapper.class, UserMapper.class})
 public interface TweetMapper {
 
     Tweet requestDtoToEntity (TweetRequestDto tweetRequestDto);
 
-    @Mapping(target = "author.username", source = "author.credentials.username")
     TweetResponseDto entityToResponseDto (Tweet tweet);
 
-    @Mapping(target = "author.username", source = "author.credentials.username")
     List<TweetResponseDto> entitiesToResponseDtos(List<Tweet> tweets);
-
-    default Long map(Timestamp timestamp) {
-        return timestamp == null ? null : timestamp.getTime();
-    } 
-
-    default Timestamp map(Long time) {
-        return time == null ? null : Timestamp.from(Instant.ofEpochSecond(time));
-    }
 }
--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/mappers/UserMapper.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/mappers/UserMapper.java
@@ -1,7 +1,5 @@
 package com.cooksys.springassessmentsocialmediasprint72022team4.mappers;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.List;
 
 import org.mapstruct.Mapper;
@@ -11,7 +9,7 @@ import com.cooksys.springassessmentsocialmediasprint72022team4.entities.User;
 import com.cooksys.springassessmentsocialmediasprint72022team4.model.UserRequestDto;
 import com.cooksys.springassessmentsocialmediasprint72022team4.model.UserResponseDto;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {TimeMapper.class})
 public interface UserMapper {
 
     User requestDtoToEntity(UserRequestDto userRequestDto);
@@ -21,13 +19,5 @@ public interface UserMapper {
 
     @Mapping(target = "username", source = "credentials.username")
     List<UserResponseDto> entitiesToResponseDtos(List<User> users);
-
-    default Long map(Timestamp timestamp) {
-        return timestamp == null ? null : timestamp.getTime();
-    } 
-
-    default Timestamp map(Long time) {
-        return time == null ? null : Timestamp.from(Instant.ofEpochSecond(time));
-    }
 
 }
