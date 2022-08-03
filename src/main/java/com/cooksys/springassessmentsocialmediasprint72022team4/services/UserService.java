0b5086d52f10bc4f88adc94b940ef5675463ad78
Jesus Milan <milan.jesusdev@gmail.com>
2022-08-02 21:44:01 -0400

    Closes #43. Implemented post new user functionality

--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/services/UserService.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/services/UserService.java
@@ -1,4 +1,8 @@
 package com.cooksys.springassessmentsocialmediasprint72022team4.services;
 
+import com.cooksys.springassessmentsocialmediasprint72022team4.model.UserRequestDto;
+import com.cooksys.springassessmentsocialmediasprint72022team4.model.UserResponseDto;
+
 public interface UserService {
+    UserResponseDto createUser(UserRequestDto userRequestDto);
 }
