cb1d9ac9122173610c2888a9282aae78e574925e
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-03 07:11:17 -0700

    Fix type conflict introduced in merge

--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/Seeder.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/Seeder.java
@@ -261,10 +261,10 @@ public class Seeder implements CommandLineRunner {
 		tweetRepository.saveAndFlush(mention1);
 
 		// Following
-		List<User> following_1 = List.of(user2, user3, user4, deletedUser);
+		Set<User> following_1 = Set.of(user2, user3, user4, deletedUser);
 		user1.setFollowing(following_1);
 
-		List<User> followers_1 = List.of(user5, deletedUser);
+		Set<User> followers_1 = Set.of(user5, deletedUser);
 		user1.setFollowers(followers_1);
 		userRepository.saveAndFlush(user1);
     }
