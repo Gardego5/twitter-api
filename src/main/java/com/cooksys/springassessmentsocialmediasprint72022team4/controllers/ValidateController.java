5c45c1f9b6774e95d947f413d3648d828696c1b4
Gardego5 <garrett.davis.7777@gmail.com>
2022-08-03 23:01:16 -0700

    Closes #47 Check whether hashtag exists

--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/controllers/ValidateController.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/controllers/ValidateController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cooksys.springassessmentsocialmediasprint72022team4.services.HashtagService;
 import com.cooksys.springassessmentsocialmediasprint72022team4.services.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class ValidateController {
 
     private final UserService userService;
+    private final HashtagService hashtagService;
 
     @GetMapping("/username/exists/@{username}")
     public Boolean usernameExists(@PathVariable String username) {
@@ -24,5 +26,10 @@ public class ValidateController {
     @GetMapping("/username/available/@{username}")
     public Boolean usernameAvailable(@PathVariable String username) {
         return userService.usernameAvailable(username);
+    }
+
+    @GetMapping("/tag/exists/{label}")
+    public Boolean tagExists(@PathVariable String label) {
+        return hashtagService.tagExists(label);
     }
 }
--- src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/services/HashtagService.java
+++ src/main/java/com/cooksys/springassessmentsocialmediasprint72022team4/services/HashtagService.java
@@ -1,4 +1,6 @@
 package com.cooksys.springassessmentsocialmediasprint72022team4.services;
 
 public interface HashtagService {
+
+    Boolean tagExists(String label);
 }
